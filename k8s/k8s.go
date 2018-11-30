38ea1a532f85c70f7dbd115e21f6cecfa26a2177
Sam Fredrickson <samf@advmicrogrid.com>
2018-11-29 17:16:58 -0800

    Add error logs to try and detect #178.

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -97,13 +97,22 @@ func (k8s *Client) PodByIP(IP string) (*v1.Pod, error) {
 	}
 
 	if len(pods) == 1 {
-		return pods[0].(*v1.Pod), nil
+		p, ok := pods[0].(*v1.Pod)
+		if !ok {
+			fmt.Errorf("pods[0] is not *v1.Pod! %v", pods[0])
+		}
+		return p, nil
 	}
 
 	//This happens with `hostNetwork: true` pods
 	podNames := make([]string, len(pods))
 	for i, pod := range pods {
-		podNames[i] = pod.(*v1.Pod).ObjectMeta.Name
+		p, ok := pod.(*v1.Pod)
+		if !ok {
+			fmt.Errorf("pods[%d] is not *v1.Pod! %v", i, pod)
+		} else {
+			podNames[i] = p.ObjectMeta.Name
+		}
 	}
 	return nil, fmt.Errorf("%d pods (%v) with the ip %s indexed", len(pods), podNames, IP)
 }
